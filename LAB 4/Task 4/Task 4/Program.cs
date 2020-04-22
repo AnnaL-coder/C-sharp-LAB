07e8a04e44049f6bf94f7ed76f94f477351700f7
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-04-22 23:17:02 +0300

    Add files via upload

---
+++ LAB 4/Task 4/Task 4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of product");
+            int n = int.Parse(Console.ReadLine());
+            Sklad[] A = new Sklad[n];
+
+            for (int i = 0; i < n; i++)
+                A[i] = new Sklad();
+            for (int i = 0; i < n; i++)
+                A[i].Inputonfo();
+
+            bool execution = true;
+
+            while (execution)
+            {
+                Console.WriteLine("\nA. Search Product by DATE");
+                Console.WriteLine("B. Search Product by PRICE");
+                Console.WriteLine("C. Search Product by SIZE");
+                Console.WriteLine("D. Exit");
+
+                string action;
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "A":
+                        Console.WriteLine("\n Enter DATETOCOME");
+                        string date = Console.ReadLine();
+                        for (int i = 0; i < n; i++)
+                            A[i].FindByDatetocome(A[i], date);
+                        break;
+                    case "B":
+                        Console.WriteLine("\n Enter  price");
+                        double Price;
+                        Price = double.Parse(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                            A[i].FindByPrice(A[i], Price);
+                        break;
+                    case "C":
+                        Console.WriteLine("\n Enter size");
+                        int Size;
+                        Size = int.Parse(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                            A[i].FindByPrice(A[i], Size);
+                        break;
+                    case "D":
+                        execution = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ LAB 4/Task 4/Task 4/Sklad.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace task4
+{
+    public class Sklad
+    {
+        private string name;
+        private string Datetocome;
+        private double price;
+        private int size;
+
+
+        public Sklad()
+        {
+        }
+
+        public void Inputonfo()
+        {
+            string name;
+            string Datetocome;
+            double price;
+            int size;
+            Console.WriteLine("\n Enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("\n Enter datetocome");
+            Datetocome = Console.ReadLine();
+            Console.WriteLine("\n Enter price");
+            price = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Enter size");
+            size = int.Parse(Console.ReadLine());
+
+            this.name = name;
+            this.Datetocome = Datetocome;
+            this.price = price;
+            this.size = size;
+        }
+
+        public void Getinfo(Sklad info)
+        {
+            Console.WriteLine("name: {0}, datetocome: {1}, price: {2}, size: {3}", name, Datetocome, price, size);
+        }
+
+        public void FindByDatetocome(Sklad info, string datetocome)
+        {
+            if (info.Datetocome == datetocome)
+            {
+                Getinfo(info);
+            }
+        }
+
+        public void FindBySize(Sklad info, int size)
+        {
+            if (info.size == size)
+                Getinfo(info);
+        }
+
+        public void FindByPrice(Sklad info, double price)
+        {
+            if (info.price == price)
+                Getinfo(info);
+        }
+
+    }
+}
+

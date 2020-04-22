e4c93711e7e078c891f27707455db7600c228d31
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-04-22 23:39:34 +0300

    Add files via upload

---
+++ LAB 5/Task 2/Task 2/Books.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5._2
+{
+    class Books
+    {
+        private string fio;
+        private string name;
+        private string code;
+        private int year;
+        private int pages;
+        private string type;
+
+        public Books()//конструктор без параметров
+        { }
+
+        public Books(string fio, string name, string code, int year,
+            int pages, string type)
+        {
+            this.fio = fio;
+            this.name = name;
+            this.code = code;
+            this.year = year;
+            this.pages = pages;
+            this.type = type;
+
+        }
+
+        public string Fio => fio;
+        public string Name => name;
+        public int Year => year;
+        public int Pages => pages;
+        public string Type => type;
+        public string Code
+        {
+            get => code;
+            set => code = value;
+        }
+
+        bool EqByFio(Books books)
+        {
+            return this.fio.Equals(books.fio);
+        }
+        bool EqByName(Books books)
+        {
+            return this.name.Equals(books.name);
+        }
+        bool EqByYear(Books books)
+        {
+            return this.year.Equals(books.year);
+        }
+        bool EqBypages(Books books)
+        {
+            return this.pages.Equals(books.pages);
+        }
+        bool EqBytype(Books books)
+        {
+            return this.type.Equals(books.type);
+        }
+        bool EqByCode(Books books)
+        {
+            return this.code.Equals(books.code);
+        }
+
+        public override string ToString()
+        {
+            return $"\n{nameof(fio)}: {fio}, {nameof(name)}: {name}, {nameof(code)}:" +
+             $" {code}, {nameof(year)}: {year}, {nameof(pages)}: {pages}, {nameof(type)}: {type}";
+        }
+    }
+}
---
+++ LAB 5/Task 2/Task 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab5._2
+{
+    class Program
+    {
+
+        static List<Books> books = new List<Books>();
+
+        private static void SearchbyCode(string code)
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books found");
+                return;
+            }
+
+            Books book = books.Find(Book => code.Equals(Book.Code));
+            Console.WriteLine($"Book with code: {code}" + book.ToString());
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Finding by code");
+            SearchbyCode("M-141");
+            books.Add(new Books("Petrov", "Poems", "I-123", 1820, 300, "Poem"));
+            books.Add(new Books("Cambridge", "English", "E-001", 2020, 120, "Studying"));
+            books.Add(new Books("Schevchenko", "Kobzar", "I-123", 1820, 300, "Poem"));
+            books.Add(new Books("Ivanov", "Autos", "M-141", 2018, 120, "Magazine"));
+            books.Add(new Books("SumDU", "Math", "S-99", 2001, 270, "Studying"));
+
+            Console.WriteLine("Finding by code");
+            SearchbyCode("M-141");
+            Console.WriteLine("Finding by code");
+            SearchbyCode("S-99");
+
+
+        }
+
+
+
+    }
+}

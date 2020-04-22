97ecd8c91845d624420a07dd3fb27a741a07524b
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-04-22 23:13:46 +0300

    Add files via upload

---
+++ LAB 4/Task 1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab4
+{
+    class A
+    {
+        public int a;
+        public int b;
+
+        public double c
+        {
+            get { if (a > b) return a - b; else return b - a; }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine("Enter a");
+            a.a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            a.b = int.Parse(Console.ReadLine());
+            var k = a.c;
+            Console.WriteLine($"Result ={k}");
+        }
+    }
+}

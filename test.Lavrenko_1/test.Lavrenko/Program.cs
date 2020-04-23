558687063a47af048eb088dd14e4d81037e236da
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-04-23 10:27:50 +0300

    Add files via upload

---
+++ test.Lavrenko_1/test.Lavrenko/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace test.Lavrenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of students");
+            int n = int.Parse(Console.ReadLine());
+            Student[] A = new Student[n];
+            for (int i = 0; i < n; i++)
+                A[i] = new Student();
+            for (int i = 0; i < n; i++)
+                A[i].InputStudent();
+
+            bool execution= true;
+
+            while (execution)
+            {   
+                Console.WriteLine("\nA. Search Student by Lastname");
+                Console.WriteLine("B. Search Student by Group");
+                Console.WriteLine("C. Exit");
+
+                string action;
+                action = Console.ReadLine();
+
+                switch(action)
+                {
+                    case "A":
+                        Console.WriteLine("\n Enter Lastname of Student");
+                        string SLastname = Console.ReadLine();
+                        for (int i = 0; i < n; i++)
+                            A[i].FindByLastname(A[i], SLastname);
+                        break;
+                    case "B":
+                        Console.WriteLine("\n Enter Group of Student");
+                        string SGroup = Console.ReadLine();
+                        for (int i = 0; i < n; i++)
+                            A[i].FindByGroup(A[i], SGroup);
+                        break;
+                    case "C":
+                        execution = false;
+                        break;
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ test.Lavrenko_1/test.Lavrenko/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace test.Lavrenko
+{
+    class Student
+    {
+        string lastname;
+        string firstname;
+        string group;
+        double average_mark;
+        string budget_or_not;
+
+        public Student()
+        {
+        }
+      
+
+        public void InputStudent()
+        {
+            string lastname;
+            string firstname;
+            string group;
+            double average_mark;
+            string budget_or_not;
+            Console.WriteLine("\n Enter lastname");                                                  
+            lastname = Console.ReadLine();
+            Console.WriteLine(" Enter firstname");
+            firstname = Console.ReadLine();
+            Console.WriteLine(" Enter group");
+            group = Console.ReadLine();
+            Console.WriteLine(" Enter average_mark");
+            average_mark = double.Parse(Console.ReadLine());
+            if(average_mark<0)
+            {
+                Console.WriteLine("Value of mark is wrong");
+            }
+            Console.WriteLine(" Enter budget_or_not(yes/no)");
+            budget_or_not = Console.ReadLine();
+
+            this.lastname = lastname;
+            this.firstname = firstname;
+            this.group = group;
+            this.average_mark = average_mark;
+            this.budget_or_not = budget_or_not;
+        }
+
+        public void Getinfo(Student info )
+        {
+            Console.WriteLine("\n Firstname:{0} Lastname: {1} Group: {2}", info.firstname, info.lastname, info.group);
+            Console.WriteLine(" Average mark: {0}, Budget: {1}", info.average_mark, info.budget_or_not);
+        }
+
+       
+        public void FindByLastname(Student info, string lastname)
+        {
+            if(info.lastname==lastname)
+            {
+                Getinfo(info);
+            }
+        }
+
+        public void FindByGroup(Student info, string group)
+        {
+            if (info.group == group)
+                Getinfo(info);
+        }
+
+
+    }
+
+
+}

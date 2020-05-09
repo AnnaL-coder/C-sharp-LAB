410da1c3d543f2d78994ad9e192220bea3ceee79
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-05-09 20:12:48 +0300

    Add files via upload

---
+++ LAB 6/Task 1/TAsk1/DemoPoint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class DemoPoint : IDemo
+    {
+
+        protected int x;
+        protected int y;
+        public DemoPoint(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Точка на плоскости: ({x}, {y})");
+        }
+
+        public double Length()
+        {
+
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public int X => x;
+
+        public int Y => y;
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i == 0) return x;
+                if (i == 1) return y;
+                throw new Exception("недопустимое значение индекса");
+            }
+            set
+            {
+                if (i == 0) x = value;
+                else if (i == 1) y = value;
+                else throw new Exception("недопустимое значение индекса");
+            }
+        }
+    }
+
+}
---
+++ LAB 6/Task 1/TAsk1/IDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    interface IDemo
+    {
+        void Show();
+        double Length();
+        int X { get; }
+        int Y { get; }
+        int this[int i] { get; set; }
+
+    }
+}

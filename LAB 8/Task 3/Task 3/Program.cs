27039d929f0bb338c09c5679da2f07ceefd6d298
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-05-15 22:34:27 +0300

    Add files via upload

---
+++ LAB 8/Task 3/Task 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        private delegate double Del(double r);
+        private static Calculations _calculations = new Calculations();
+        static void Main(string[] args)
+        {
+            var r = 0.0;
+            Console.WriteLine("Input R of circle");
+            r = double.Parse(Console.ReadLine());
+            if (r < 0) throw new ArgumentException();
+
+            Del del = _calculations.Volume;
+            Console.WriteLine($"Volume of circle is calculated. Result = {del(r)}");
+            del = _calculations.Square;
+            Console.WriteLine($"Square of circle is calculated. Result = {del(r)}");
+            del = _calculations.Length;
+            Console.WriteLine($"Lenght of circle is calculated. Result = {del(r)}");
+        }
+    }
+
+}

1894ca88f1f10dee3eb235f97b6376542817e55b
AnnaL-coder <64162341+AnnaL-coder@users.noreply.github.com>
2020-05-15 22:34:09 +0300

    Add files via upload

---
+++ LAB 8/Task 3/Task 3/Calculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Calculations
+    { 
+
+    public double Square(double r)
+    {
+        return Math.PI * Math.Pow(r, 2);
+    }
+    public double Length(double r)
+    {
+        return 2 * Math.PI * r;
+    }
+
+    public double Volume(double r)
+    {
+        return (4.0 / 3) * Math.PI * Math.Pow(r, 3);
+    }
+
+}
+}

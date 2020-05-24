fab08274863b87569520c0a62ef5898101c7c33d
AnnaLavrenko <64162341+AnnaLavrenko@users.noreply.github.com>
2020-05-24 14:17:30 +0300

    Add files via upload

---
+++ kr2/kr2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kr2
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            
+
+            List<Items> items = new List<Items>()
+            {
+                new Items("item1", 5, 5, 10),
+                new Items("item2", 3, 15, 30),
+                new Items("item3", 4, 65, 20),
+                new Items("item4", 1, 57, 90)
+            };
+
+            Sorter.SortItems("price",items);
+            Console.WriteLine("Sorted by price: ");
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i].Price);
+            }
+            Console.WriteLine("Sorted by count: ");
+            Sorter.SortItems("count", items);
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i].Count);
+            }
+            Console.WriteLine("Sorted by Expiration Days: ");
+            Sorter.SortItems("expirationDay", items);
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i].ExpirationDays);
+            }
+        }
+
+    }
+}
---
+++ kr2/kr2/Sorter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kr2
+{
+    class Sorter
+    {
+        public delegate List<Items> UseSort(List<Items> items);
+        public static Dictionary<string, UseSort> dictionary = new Dictionary<string, UseSort> {
+            { "name", NameSort },
+            { "price", PriceSort },
+            { "count", CountSort },
+            { "expirationDay", ExpDaySort },
+        };
+
+        public static List<Items> SortItems(string crit, List<Items> items)
+        {
+            return dictionary[crit](items);
+        }
+
+        public static List<Items> PriceSort(List<Items> items)
+        {
+            Items t;
+            for (int p = 0; p < items.Count() - 2; p++)
+            {
+                for (int i = 0; i <= items.Count() - 2; i++)
+                {
+                    if (items[i].Price > items[i + 1].Price)
+                    {
+                        t = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = t;
+                    }
+                }
+            }
+            return items;
+        }
+
+        public static List<Items> CountSort(List<Items> items)
+        {
+            Items t;
+            for (int p = 0; p < items.Count() - 2; p++)
+            {
+                for (int i = 0; i <= items.Count() - 2; i++)
+                {
+                    if (items[i].Count > items[i + 1].Count)
+                    {
+                        t = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = t;
+                    }
+                }
+            }
+            return items;
+        }
+
+        public static List<Items> ExpDaySort(List<Items> items)
+        {
+            Items t;
+            for (int p = 0; p < items.Count() - 2; p++)
+            {
+                for (int i = 0; i <= items.Count() - 2; i++)
+                {
+                    if (items[i].ExpirationDays > items[i + 1].ExpirationDays)
+                    {
+                        t = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = t;
+                    }
+                }
+            }
+            return items;
+        }
+
+        public static List<Items> NameSort(List<Items> items)
+        {
+            return null;
+        }
+    }
+}
